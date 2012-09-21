575126fa5cd0b4cb8ad369d9641ddec9b9f33e76
HealthyPrice <arvindvyas07@gmail.com>
2012-09-21 20:44:36 +0530

    comp static test

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-SampleApp::Application.load_tasks
+Arvind::Application.load_tasks
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arvind::Application.config.secret_token = '0020e92bddde3ea8e03445cd5b0a2d94814aad18eaee7e919c40de0a39b17b54640156c23c5c2a817fd15ba3949bfc652120258adec3767cd78a1efc9f42bbba'
