b4df64c2dc6a90832f184d4599b3e6985135946e
HealthyPrice <arvindvyas07@gmail.com>
2012-09-22 16:12:49 +0530

    remove repetation

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,13 +3,18 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+
+  before(:each) do
+
+  @base_title = "Ruby on Rails Tutorial Sample App"
+   
+  end
   
 
   describe  "GET 'home'" do
-    it "should   have title" do
+    it "shoud have the correct title" do
       get 'home'
-        response.should have_selector("title",
-                    :content => "Ruby on Rails Tutorial Sample App | Home")
+        response.should have_selector("title",:content => @base_title +" | Home")
     end
   end
   
@@ -28,10 +33,10 @@ describe PagesController do
   end
 
   describe "GET 'contact'" do
-    it "should have title" do
+    it "shoud have the correct title" do
       get 'contact'
         response.should have_selector("title",
-            :content => "Ruby on Rails Tutorial Sample App | Contact")
+            :content => @base_title +  " | Contact")
       end
     end
 
@@ -42,17 +47,17 @@ describe PagesController do
     end
 
     describe "GET 'about'" do
-      it "should have the title" do
+      it "shoud have the correct title" do
         get 'about'
         response.should have_selector("title",
-                 :content => "Ruby on Rails Tutorial Sample App | About")
+                 :content =>  @base_title + " | About")
       end
     end
     describe "GET 'help'" do
       it "shoud have the correct title" do
         get 'help'
         response.should have_selector("title",
-                                         :content => "Ruby on Rails Tutorial Sample App | Help")
+                                         :content =>@base_title +" | Help")
       end
     end
 
