455450d1b2ca71a63db1a9a7547550a5efbffa73
HealthyPrice <arvindvyas07@gmail.com>
2012-10-06 19:04:09 +0530

    chage in

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,4 +16,20 @@ describe UsersController do
    	  response.should have_selector("title",:content =>'Sign up')
     end
   end
+  describe "GET 'show'" do
+     it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+  end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+	user.name = "Arvind vyas"
+	user.email = "arvindvyas07@gmail.com"
+	user.password = "foobar"
+	user.password_confirmation = "foobar"
+end
